f1c8ca195624c0d0b0945bcf73c0e7d7e8df2343
DiptoChakrabarty <diptochuck123@gmail.com>
2021-11-29 00:30:10 +0530

    use pytest fixtures

--- test_bank.py
+++ test_bank.py
@@ -1,26 +1,31 @@
 import pytest
 from BankCode import Bank,InsufficientBalance
 
-def test_default_account():
-    account = Bank()
-    assert account.balance == 100
+@pytest.fixture
+def default_account():
+    # Creates the default Bank Account
+    return Bank()
+
+@pytest.fixture
+def balance_account():
+    # Creates Bank Account with balance 500
+    return Bank(500)
+
+def test_default_account(default_account):
+    assert default_account.balance == 100
     
-def test_set_account_balance():
-    account = Bank(500)
-    assert account.balance == 500
+def test_set_account_balance(balance_account):
+    assert balance_account.balance == 500
 
-def test_add_balance():
-    account = Bank(500)
-    account.add_money(1000)
-    assert account.balance == 1500
+def test_add_balance(balance_account):
+    balance_account.add_money(1000)
+    assert balance_account.balance == 1500
 
-def test_withdraw_balance():
-    account = Bank(500)
-    account.withdraw_money(300)
-    assert account.balance == 200
+def test_withdraw_balance(balance_account):
+    balance_account.withdraw_money(300)
+    assert balance_account.balance == 200
 
-def test_exceed_withdraw_amount():
-    account = Bank(500)
+def test_exceed_withdraw_amount(balance_account):
     with pytest.raises(InsufficientBalance):
-        account.withdraw_money(1000)
+        balance_account.withdraw_money(1000)
     
