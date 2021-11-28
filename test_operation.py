d48363ae7bcab541b5c94e858467b78339f748b1
DiptoChakrabarty <diptochuck123@gmail.com>
2021-11-28 22:22:12 +0530

    initial addition test

---
+++ test_operation.py
@@ -0,0 +1,12 @@
+import pytest
+from opeartion import addition
+
+def test_add_positive():
+    assert addition(5,6) == 11
+
+def test_add_negative():
+    assert addition(-3,-2) == -5
+
+def test_add_strings():
+    with pytest.raises(TypeError):
+        addition(5,"Invalid")
