6df05709271b0d672ac031b7470352f246d3d9e2
DiptoChakrabarty <diptochuck123@gmail.com>
2021-11-29 01:14:35 +0530

    pytest parameterize

--- test_bank.py
+++ test_bank.py
@@ -28,4 +28,13 @@ def test_withdraw_balance(balance_account):
 def test_exceed_withdraw_amount(balance_account):
     with pytest.raises(InsufficientBalance):
         balance_account.withdraw_money(1000)
+
+@pytest.mark.parametrize("add_amount,withdraw_amount,expected",[
+    (1000,700,800),
+    (200,400,300)
+])
+def test_account_operations(balance_account,add_amount,withdraw_amount,expected):
+    balance_account.add_money(add_amount)
+    balance_account.withdraw_money(withdraw_amount)
+    assert balance_account.balance == expected
     
