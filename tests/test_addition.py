3f5495ac86dced83946386f1d5f461696eae9466
DiptoChakrabarty <diptochuck123@gmail.com>
2021-12-05 10:47:23 +0530

    fix flake issues

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,12 +1,15 @@
 import pytest
 from Operation.addition import addition
 
+
 def test_add_positive():
-    assert addition(5,6) == 11
+    assert addition(5, 6) == 11
+
 
 def test_add_negative():
-    assert addition(-3,-2) == -5
+    assert addition(-3, -2) == -5
+
 
 def test_add_strings():
     with pytest.raises(TypeError):
-        addition(5,"Invalid")
+        addition(5, "Invalid")
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,40 +1,47 @@
 import pytest
-from Operation.BankCode import Bank,InsufficientBalance
+from Operation.BankCode import Bank, InsufficientBalance
+
 
 @pytest.fixture
 def default_account():
     # Creates the default Bank Account
     return Bank()
 
+
 @pytest.fixture
 def balance_account():
     # Creates Bank Account with balance 500
     return Bank(500)
 
+
 def test_default_account(default_account):
     assert default_account.balance == 100
-    
+
+
 def test_set_account_balance(balance_account):
     assert balance_account.balance == 500
+
 
 def test_add_balance(balance_account):
     balance_account.add_money(1000)
     assert balance_account.balance == 1500
 
+
 def test_withdraw_balance(balance_account):
     balance_account.withdraw_money(300)
     assert balance_account.balance == 200
+
 
 def test_exceed_withdraw_amount(balance_account):
     with pytest.raises(InsufficientBalance):
         balance_account.withdraw_money(1000)
 
-@pytest.mark.parametrize("add_amount,withdraw_amount,expected",[
-    (1000,700,800),
-    (200,400,300)
+
+@pytest.mark.parametrize("add_amount,withdraw_amount,expected", [
+    (1000, 700, 800),
+    (200, 400, 300)
 ])
-def test_account_operations(balance_account,add_amount,withdraw_amount,expected):
+def test_account_operations(balance_account, add_amount, withdraw_amount, expected):
     balance_account.add_money(add_amount)
     balance_account.withdraw_money(withdraw_amount)
     assert balance_account.balance == expected
-    
